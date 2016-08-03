042eecda5177fdee86004122247626e65e40db63
zhaowenjian <271118105@qq.com>
2016-08-03 17:29:40 +0800

    gomoku

---
+++ public/gomoku/js/I18N/en.js
@@ -0,0 +1,53 @@
+J42R.put("en", {
+    "gamewon": {
+         "win" : "You win!",
+         "again" : "You win! Play again?",
+         "back" : "Back",
+         "startnew" : "New Game"
+    },
+    "newgame" : {
+         "newgame" : "New Game",
+         "with" : "Play with",
+         "human" : "Human",
+         "computer" : "Computer",
+         "color" : "Your Color",
+         "black" : "Black",
+         "white" : "White",
+         "level" : "Level",
+         "novice" : "Novice",
+         "medium" : "Medium",
+         "expert" : "Expert",
+         "back" : "Back",
+         "start" : "Start"
+    },
+    "help" : {
+         "about" : "About",
+         "enjoy" : "Enjoy!",
+         "development" : "Mobile Development: Liming Xie",
+         "design" : "Original Design: Yujian Yao",
+         "ad" : "RNJSOFT, offering customization service on Mobile Apps and Cloud Computing soutions.",
+         "welcome" : "If you need own mobile apps & games, contact us.",
+         "email" : "Email: rnjsoft.mobile@gmail.com",
+         "twitter" : "Twitter: @rnjsoft",
+         "website" : "Website: http://www.rnjsoft.com/"
+    },
+    "game" : {
+         "gomoku" : "Gomoku",
+         "yourturn" : "Your turn.",
+         "new" : "New",
+         "undo" : "Undo",
+         "about" : "About"
+    },
+    "msg" : {
+         "uwon" : "You won!",
+         "ulost" : "You lost!",
+         "ulostagain" : "Oops! You lost. Play again?",
+         "uwonagain" : "You won. Play again?",
+         "yourturn" : "Your turn.",
+         "thinking" : "Thinking...",
+         "wonagain" : "Won. Play again?",
+         "pcwon" : "Computer won.",
+         "won" : "Win!",
+         "human" : "Human"
+    }
+});
---
+++ public/gomoku/js/I18N/zh.js
@@ -0,0 +1,53 @@
+J42R.put("zh", {
+    "gamewon": {
+         "win" : "你赢了！",
+         "again" : "你已经赢了！再来吗？",
+         "back" : "返回",
+         "startnew" : " 开始新游戏 "
+    },
+    "newgame" : {
+         "newgame" : "新游戏",
+         "with" : "选择对手",
+         "human" : "玩家",
+         "computer" : "电脑",
+         "color" : "选择颜色",
+         "black" : "黑棋",
+         "white" : "白棋",
+         "level" : "选择难度",
+         "novice" : "新手上路",
+         "medium" : "实力不凡",
+         "expert" : "高深莫测",
+         "back" : "返回",
+         "start" : "开始"
+    },
+    "help" : {
+         "about" : "关于",
+         "enjoy" : "祝玩得开心！",
+         "development" : "移动版本开发：谢黎明",
+         "design" : "原始设计：姚宇健",
+         "ad" : "智能手机应用、云计算解决方案，安杰科技为您量身打造。",
+         "welcome" : "如有定制需要，请与我们联系。",
+         "twitter" : "新浪微博：@RNJSOFT安杰科技 ｜ 微信：RNJSOFT",
+         "website" : "官方网站：http://www.rnjsoft.com/",
+         "email" : "邮箱：rnjsoft.mobile@gmail.com"
+    },
+    "game" : {
+         "gomoku" : "五子棋",
+         "yourturn" : "该你走",
+         "new" : "新游戏",
+         "undo" : "撤销",
+         "about" : "更多..."
+    },
+    "msg" : {
+         "uwon" : "你赢了！",
+         "ulost" : "你输了！",
+         "ulostagain" : "啊哦！你输给电脑了。再来吗？",
+         "uwonagain" : "你赢了。再来吗？",
+         "yourturn" : "该你走",
+         "thinking" : "思考中...",
+         "wonagain" : "赢了。再来吗？",
+         "pcwon" : "电脑赢了。",
+         "won" : "赢了！",
+         "human" : "玩家"
+    }
+});
