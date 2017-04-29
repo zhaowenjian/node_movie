5e60b5d24150818a2beaaf716648368ce79f6d09
zhaowenjian <zhaowenjian@github.com>
2017-04-29 18:40:23 +0800

    trace

--- public/telematics_temp/js/trace.js
+++ public/telematics_temp/js/trace.js
@@ -360,7 +360,7 @@ window.onload = function(){
 					var circle=new qq.maps.Circle({
 					       	map:map,
 					       	center: point,
-					       	radius: 7,
+					       	radius: 8,
 					       	fillColor: circleColor,
 					       	strokeColor: circleColor,
 					       	strokeWeight:1,
